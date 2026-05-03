223813dbb67e54fcdbf2a1388f958a53d8e99884
ClarkPower <cbakstran@gmail.com>
2026-05-03 15:00:20 -0400

    rewind effects

--- Assets/Scripts/UI/TimeDirectionDisplay.cs
+++ Assets/Scripts/UI/TimeDirectionDisplay.cs
@@ -7,11 +7,15 @@ namespace TimeTravelBanana.UI
 {
     public class TimeDirectionDisplay : MonoBehaviour
     {
-        [SerializeField] private int fontSize = 36;
-        [SerializeField] private float bottomOffset = 30f;
+        [SerializeField] private int defaultFontSize = 36;
+        [SerializeField] private int rewindFontSize = 36;
+        [SerializeField] private float defaultBottomOffset = 30f;
+        [SerializeField] private float rewindBottomOffset = 30f;
         [SerializeField] private Vector2 rewindImageSize = new Vector2(480f, 80f);
         [SerializeField] private float rewindImageOffsetY = 90f;
         [SerializeField] private float rewindFps = 8f;
+        [SerializeField] private Color fontColor = Color.red;
+        [SerializeField] private float rewindBlinkPeriod = 0.6f;
 
         private Text label;
         private string lastText;
@@ -21,6 +25,10 @@ namespace TimeTravelBanana.UI
         private float frameTimer;
         private int frameIndex;
 
+        private float blinkTimer;
+        private bool isRewindActive;
+
+        private RectTransform rt;
         private void Awake()
         {
             var canvas = GetComponentInParent<Canvas>();
@@ -28,22 +36,23 @@ namespace TimeTravelBanana.UI
             if (canvas == null) { Debug.LogWarning("TimeDirectionDisplay: no Canvas in scene; UI not built."); return; }
 
             var go = new GameObject("TimeDirectionText", typeof(RectTransform), typeof(Text));
-            var rt = (RectTransform)go.transform;
+            rt = (RectTransform)go.transform;
             rt.SetParent(canvas.transform, false);
             rt.anchorMin = new Vector2(0.5f, 0f);
             rt.anchorMax = new Vector2(0.5f, 0f);
             rt.pivot = new Vector2(0.5f, 0f);
             rt.sizeDelta = new Vector2(360f, 56f);
-            rt.anchoredPosition = new Vector2(0f, bottomOffset);
 
             label = go.GetComponent<Text>();
             label.font = Resources.GetBuiltinResource<Font>("LegacyRuntime.ttf");
-            label.fontSize = fontSize;
             label.fontStyle = FontStyle.Bold;
             label.alignment = TextAnchor.MiddleCenter;
-            label.color = Color.red;
+            label.color = fontColor;
             label.raycastTarget = false;
+            label.verticalOverflow = VerticalWrapMode.Overflow;
+            label.horizontalOverflow = HorizontalWrapMode.Overflow;
             label.text = "";
+            SetDefaultLabelProperties();
 
             rewindFrames = new Sprite[4];
             for (int i = 0; i < 4; i++)
@@ -56,7 +65,8 @@ namespace TimeTravelBanana.UI
             irt.anchorMax = new Vector2(0.5f, 0f);
             irt.pivot = new Vector2(0.5f, 0f);
             irt.sizeDelta = rewindImageSize;
-            irt.anchoredPosition = new Vector2(0f, bottomOffset + rewindImageOffsetY);
+            irt.anchoredPosition = new Vector2(0f, rewindImageOffsetY);
+
 
             rewindImage = imgGo.GetComponent<Image>();
             rewindImage.raycastTarget = false;
@@ -64,6 +74,18 @@ namespace TimeTravelBanana.UI
             if (rewindFrames[0] != null) rewindImage.sprite = rewindFrames[0];
             else Debug.LogWarning("TimeDirectionDisplay: could not load Resources/Art/rewind/frame 1.");
             imgGo.SetActive(false);
+        }
+        void SetDefaultLabelProperties()
+        {
+            label.fontSize = defaultFontSize;
+            rt.anchoredPosition = new Vector2(0f, defaultBottomOffset); 
+        }
+
+        void SetRewindLabelProperties()
+        {
+            label.fontSize = rewindFontSize;
+            rt.anchoredPosition = new Vector2(0f, rewindBottomOffset); 
+            
         }
 
         private void Update()
@@ -87,6 +109,10 @@ namespace TimeTravelBanana.UI
 
             if (next != lastText)
             {
+                if(next == "<<RR")
+                    SetRewindLabelProperties();
+                else
+                    SetDefaultLabelProperties();
                 label.text = next;
                 lastText = next;
                 if (rewindImage != null)
@@ -99,6 +125,25 @@ namespace TimeTravelBanana.UI
                         frameTimer = 0f;
                         rewindImage.sprite = rewindFrames[0];
                     }
+                }
+
+                bool nowRewind = next == "<<RR";
+                if (nowRewind != isRewindActive)
+                {
+                    isRewindActive = nowRewind;
+                    blinkTimer = 0f;
+                    label.enabled = true;
+                }
+            }
+
+            if (isRewindActive && rewindBlinkPeriod > 0f)
+            {
+                blinkTimer += Time.deltaTime;
+                float halfPeriod = rewindBlinkPeriod * 0.5f;
+                while (blinkTimer >= halfPeriod)
+                {
+                    blinkTimer -= halfPeriod;
+                    label.enabled = !label.enabled;
                 }
             }
 
